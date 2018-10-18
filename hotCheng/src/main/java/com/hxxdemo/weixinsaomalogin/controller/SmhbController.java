db696e9966804f8d3b057d173838ec27399ae454
shywang <wangshao822@163.com>
2018-10-18 13:10:27 +0800

    微信红包返现代码的修改

--- hotCheng/src/main/java/com/hxxdemo/weixinsaomalogin/controller/SmhbController.java
+++ hotCheng/src/main/java/com/hxxdemo/weixinsaomalogin/controller/SmhbController.java
@@ -29,9 +29,10 @@ public class SmhbController {
 	public Map<String,String> sendHb(HttpServletRequest request, HttpServletResponse response) throws KeyManagementException, UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, DocumentException{
     	Map<String,String> map =new HashMap<String,String>();
     	  MD5 md=new MD5();
-    	  String oi=request.getParameter("opendid");
-    	  String opendid="od1hc1ezHucolmlIPfcFiMFvKDy8"; //openid
-    	  String totalAmount="100";//红包金额，单位时候分
+    	  String opendid=request.getParameter("opendid");
+    	  System.out.println("opendid-- smhb--->"+opendid);
+    	 // String opendid="od1hc1ezHucolmlIPfcFiMFvKDy8"; //openid
+    	  String totalAmount="100";//红包金额，单位是分
     	  String hbname="电池扫描返现";//红包名称
     	  String zfy="恭喜发财";
     	  map=  md.sendRedPack(opendid,totalAmount,hbname,zfy);
